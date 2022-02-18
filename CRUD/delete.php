930440783a8f35d4a58701c13694b84974434ea8
samouchka1 <82068124+samouchka1@users.noreply.github.com>
2022-02-18 10:58:08 -0500

    Add files via upload

---
+++ CRUD/delete.php
@@ -0,0 +1,131 @@
+<?php
+// Process delete operation after confirmation
+if(isset($_POST["id"]) && !empty($_POST["id"])){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a delete statement
+    $sql = "DELETE FROM employees WHERE id = :id";
+    
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":id", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_POST["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            // Records deleted successfully. Redirect to landing page
+            header("location: index.php");
+            exit();
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+    // Close statement
+    unset($stmt);
+    
+    // Close connection
+    unset($pdo);
+} else{
+    // Check existence of id parameter
+    if(empty(trim($_GET["id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+//TEST for READ
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+
+//================================================================================MIGHT BE ABLE TO SPLIT THIS
+    // Prepare a select statement
+    $sql = "SELECT * FROM employees WHERE id = :id";
+    
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":id", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            if($stmt->rowCount() == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+                // Retrieve individual field value
+                $name = $row["name"];
+                $address = $row["address"];
+                $salary = $row["salary"];
+            } else{
+                // URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+        // Close statement
+        unset($stmt);
+        
+        // Close connection
+        unset($pdo);
+    } else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+}
+//===========================================================================================================
+//TEST for READ
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Record</title>
+    
+    <!--Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="font-bold text-lg text-center mt-6">Delete employee data</div>
+    <!--TEST for READ-->
+    <div class="w-full md:w-3/5 lg:w-2/5 border-solid border-2 text-left mx-auto my-10">
+        <div class="text-left flex flex-col gap-3 pl-20">
+            <label class="font-bold">Name</label>
+                <p><?php echo $row["name"]; ?></p>
+            <label class="font-bold">Address</label>
+                <p><?php echo $row["address"]; ?></p>
+            <label class="font-bold">Salary</label>
+                <p>$<?php echo number_format($row["salary"]); ?></p>
+        </div>
+    <!-- </div> -->
+    <!--TEST for READ-->
+    <!-- <div class="w-2/5 border-solid border-2 text-left mx-auto my-6">  TEST-->
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="text-left flex flex-col gap-3 pl-20">
+                <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                <p class="mt-10 text-red-500">Are you sure you want to delete this employee record?</p>
+                
+            <div class="flex mt-2 gap-3">
+                <!-- <input type="hidden" name="id" value="<?php echo $id; ?>"/> -->
+                <button type="submit" class="border-solid border-2 border-neutral-900 w-20 mr-2 bg-gray-300 hover:bg-neutral-100" value="yes">Delete</button>
+                <a href="index.php"><button id="goBack" class="border-solid border-2 border-neutral-900 w-20 mr-2 bg-gray-300 hover:bg-neutral-100">Cancel</button></a>
+            </div>
+        </form>
+    </div> <!--<<<<-->
+    <script>document.getElementById('goBack').addEventListener('click', function() {history.back();})</script>
+</body>
+</html>
---
+++ CRUD/update.php
@@ -0,0 +1,162 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = "";
+$name_err = $address_err = $salary_err = "";
+ 
+// Processing form data when form is submitted
+if(isset($_POST["id"]) && !empty($_POST["id"])){ //THIS CHANGED FROM CREATE.PHP <<<<<<<<<<<
+    // Get hidden input value
+    $id = $_POST["id"];
+    
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+        $name_err = "Please enter a name.";
+    } elseif(!filter_var($input_name, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $name_err = "Please enter a valid name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate address address
+    $input_address = trim($_POST["address"]);
+    if(empty($input_address)){
+        $address_err = "Please enter an address.";     
+    } else{
+        $address = $input_address;
+    }
+    
+    // Validate salary
+    $input_salary = trim($_POST["salary"]);
+    if(empty($input_salary)){
+        $salary_err = "Please enter the salary amount.";     
+    } elseif(!ctype_digit($input_salary)){
+        $salary_err = "Please enter a positive integer value.";
+    } else{
+        $salary = $input_salary;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err)){
+
+        // Prepare an update statement
+        $sql = "UPDATE employees SET name=:name, address=:address, salary=:salary WHERE id=:id"; //SQL CODE; UPDATE, SET WHERE id=:id <<<<<<<<<<<<
+ 
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":name", $param_name);
+            $stmt->bindParam(":address", $param_address);
+            $stmt->bindParam(":salary", $param_salary);
+            $stmt->bindParam(":id", $param_id);  //THIS PARAM GETS ADDED <<<<<<<<<<<
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Records updated successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        unset($stmt);
+    }
+    
+    // Close connection
+    unset($pdo);
+
+} else{                         //ELSE <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    // Check existence of id parameter before processing further
+    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+        // Get URL parameter
+        $id =  trim($_GET["id"]);
+        
+        // Prepare a select statement
+        $sql = "SELECT * FROM employees WHERE id = :id";
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":id", $param_id);
+            
+            // Set parameters
+            $param_id = $id;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                
+                if($stmt->rowCount() == 1){             //IF THERE IS A ROW <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+                    /* Fetch result row as an associative array. Since the result set
+                    contains only one row, we don't need to use while loop */
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                
+                    // Retrieve individual field value
+                    $name = $row["name"];
+                    $address = $row["address"];
+                    $salary = $row["salary"];
+                } else{
+                    // URL doesn't contain valid id. Redirect to error page
+                    header("location: error.php");
+                    exit();
+                }
+                
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        unset($stmt);
+        
+        // Close connection
+        unset($pdo);
+    }  else{
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Update Record</title>
+
+    <!--Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+
+    <div class="font-bold text-lg text-center mt-6">Edit employee data</div>
+    <div class="w-full md:w-3/5 lg:w-2/5 border-solid border-2 text-left mx-auto my-6">
+        <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post" class="text-left flex flex-col gap-3 pl-20">
+            <label class="mx-4 font-bold">Name</label>
+                <input type="text" name="name" class="w-3/4 mx-4  border-solid border-2" value="<?php echo $name; ?>" required>
+            <label class="mx-4 font-bold">Address</label>
+                <textarea name="address" class="w-3/4 mx-4  border-solid border-2" required><?php echo $address; ?></textarea>
+            <label class="mx-4 font-bold">Salary</label>
+                <input type="text" name="salary" class="w-3/4 mx-4  border-solid border-2" value="<?php echo $salary; ?>" required>
+
+            <div class="flex mx-4 mt-2 gap-3">
+                <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                <button type="submit" class="border-solid border-2 border-neutral-900 w-20 mr-2 bg-gray-300 hover:bg-neutral-100" value="Submit">Submit</button>
+                <a href="index.php"><button class="border-solid border-2 border-neutral-900 w-20 mr-2 bg-gray-300 hover:bg-neutral-100">Cancel</button></a>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
