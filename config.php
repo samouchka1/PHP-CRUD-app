347d843a12eedcb087f60e35682f56b6926576e7
samouchka1 <82068124+samouchka1@users.noreply.github.com>
2022-08-23 15:53:15 -0400

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'test');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'crud');
  
 /* Attempt to connect to MySQL database */
